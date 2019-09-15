617473caf75c16e03b2db9c724efa93c7f37da35
Maddie <maddiemcampbell@gmail.com>
2019-09-15 11:47:33 -0700

    took out parameter index

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.getJSON('data.json', function(data){
-        $.each(data, function (index, value){
+        $.each(data, function (value){
             $("table").append("<tr><td>" 
             + value.name + "</td><td>" 
             + value.description + "</td><td><a href=https://www.google.com/maps?q=" 
